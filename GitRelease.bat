f358a4452c28bafee3365168b05d1a3fd761a8d2
stackprobe <stackprobes@gmail.com>
2018-10-03 23:45:56 +0900

    coffee break

--- GitRelease.bat
+++ GitRelease.bat
@@ -1,5 +1,5 @@
 IF NOT EXIST .\GitRelease.bat GOTO END
 CALL qq
-C:\Factory\SubTools\GitFactory.exe /ow . C:\home\GitHub\natsuconv
+C:\Factory\SubTools\GitFactory.exe /ow . C:\huge\GitHub\natsuconv
 rem CALL C:\var\go.bat
 :END
