b65413c98addc3e86e2f8bce98954a8ca97200d1
stackprobe <stackprobes@gmail.com>
2020-04-30 02:29:36 +0900

    Cosmo

--- AutoRelease.bat
+++ AutoRelease.bat
@@ -1,26 +1,22 @@
-IF NOT EXIST natsuconv\. GOTO END
 CLS
 rem リリースして qrum します。
 PAUSE
 
+CALL newcsrr
+
 CALL ff
 cx **
-
 CD /D %~dp0.
-
-IF NOT EXIST natsuconv\. GOTO END
 
 CALL qq
 cx **
 
 CALL _Release.bat /-P
 
-MOVE out\natsuconv.zip S:\リリース物\.
+MOVE out\* S:\リリース物\.
 
 START "" /B /WAIT /DC:\home\bat syncRev
 
 CALL qrumauto rel
 
 rem **** AUTO RELEASE COMPLETED ****
-
-:END
